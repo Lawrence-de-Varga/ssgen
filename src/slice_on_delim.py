d5f40873b9f4063c745074241e33a35b36da5288
Lawrence de Varga <lawdevarga@gmail.com>
2025-06-01 22:35:07 +1000

    added mstarts_with

--- src/slice_on_delim.py
+++ src/slice_on_delim.py
@@ -30,6 +30,14 @@ def starts_with(sub_string: str, string: str) -> bool:
     return True
 
 
+@type_check_decorator([list, str])
+def mstarts_with(sub_strings: list, string: str) -> str | bool:
+    for ss in sub_strings:
+        if starts_with(ss, string):
+            return ss
+    return False
+
+
 @type_check_decorator([str, str])
 def msplit(sub_string: str, string: str) -> list[str]:
     if sub_string == "":
@@ -56,6 +64,42 @@ def msplit(sub_string: str, string: str) -> list[str]:
                 current_string = ""
                 partner = not partner
                 i += len(sub_string)
+
+        else:
+            current_string += string[i]
+            i += 1
+    split_string.append(current_string)
+
+    return split_string
+
+
+@type_check_decorator([list, str])
+def mmsplit(sub_strings: list[str], string: str) -> list[str]:
+    if sub_strings == "":
+        return string
+
+    if sub_strings not in string:
+        return string
+
+    split_string: list[str] = []
+    current_string: str = ""
+    pairs = []
+
+    partner = False
+
+    i = 0
+    while not i >= len(string):
+        if starts_with(sub_strings, string[i:]):
+            if not partner:
+                split_string.append(current_string)
+                current_string = sub_strings
+                partner = not partner
+                i += len(sub_strings)
+            else:
+                split_string.append(current_string + sub_strings)
+                current_string = ""
+                partner = not partner
+                i += len(sub_strings)
 
         else:
             current_string += string[i]
